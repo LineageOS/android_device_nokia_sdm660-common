6c2779e05efea8f5ac897b7912b962fc7cd05ec2
theimpulson <aayushgupta219@gmail.com>
2019-12-27 03:51:55 +0000

    sdm660-common: Inherit QCOM sepolicy
    *Also drop our local one as most of that is defined in QCOM sepolicy
    
    Change-Id: I0a064079ae657d6995a91e03b371cff40a20b8fe

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -120,20 +120,10 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 # Telephony
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
-# SELinux
-PRIVATE_EXCLUDE_BUILD_TEST := true
-
 # Sepolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/private \
-    device/qcom/sepolicy/qva/private \
-    device/lineage/sepolicy/qcom/system
-
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/public \
-    device/qcom/sepolicy/qva/public
+BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
