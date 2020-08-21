8bf472d3d0e299f3127ebcb396a25621ac27e425
Weilun Du <wdu@google.com>
2020-08-21 14:45:14 -0700

    sdm660-common: Add TetheringOverlay
    BUG: 163800715
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: Ia0834f5215ff8c3b13effa591bd2f84d93419a06

--- sdm660.mk
+++ sdm660.mk
@@ -442,6 +442,7 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     libwpa_client \
     hostapd \
+    TetheringConfigOverlay \
     WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
