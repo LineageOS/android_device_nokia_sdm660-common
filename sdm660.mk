863892980e0c7a58e1aff009ca0c0138de176ab7
luk1337 <priv.luk@gmail.com>
2020-09-30 13:16:29 +0800

    sdm660-common: Build our own lights HAL
    Change-Id: I54ca7a639dd08ded3f4222f4530a8811093ba524

--- sdm660.mk
+++ sdm660.mk
@@ -253,9 +253,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.sdm660
+    android.hardware.light@2.0-service.nokia_sdm660
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
