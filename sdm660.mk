6b7ce7dae330d214c5b04cce6c92fd29830218f9
Aayush Gupta <aayushgupta219@gmail.com>
2020-10-14 13:55:40 +0000

    sdm660-common: Re-arrange & drop non-required proprietary-files from Misc stack
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: If4dccd1db3dd72020639b63a71169897902e454a

--- sdm660.mk
+++ sdm660.mk
@@ -159,12 +159,13 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    camera.device@3.2-impl \
-    libstdc++.vendor \
-    libxml2 \
-    Snap
+    android.hardware.camera.provider@2.5 \
+    Snap \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 PRODUCT_PACKAGES += \
     libshim_megvii
