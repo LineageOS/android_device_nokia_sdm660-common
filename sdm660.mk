9462bb202021570deffe74413deaaa066e3968d1
Aayush Gupta <aayushgupta219@gmail.com>
2020-10-21 10:58:53 +0530

    sdm660-common: Uprev to android.hardware.biometrics.fingerprint@2.2
    Ref:
    [0]: https://github.com/LineageOS/android_hardware_interfaces/commit/671c59917d89e66c20ef1549522b3ac8502a9dc3
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: If88ea3bdf6d88e2f262d0fda0399258fd5108409

--- sdm660.mk
+++ sdm660.mk
@@ -204,7 +204,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.2-service
 
 # FM
 PRODUCT_PACKAGES += \
