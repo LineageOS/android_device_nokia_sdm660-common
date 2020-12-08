3bcc456fbfae38a675f45d6deb57c5514224bb8b
Aayush Gupta <aayushgupta219@gmail.com>
2020-12-08 10:55:25 +0100

    sdm660-common: Explicitly build audio.service
    Change-Id: I7413a486bf8f7fa40e42111ea200b8b807fa5ca8

--- sdm660.mk
+++ sdm660.mk
@@ -103,6 +103,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
+    android.hardware.audio.service \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
