f88079fd3e42bb5b98970b7ab062bc9037e672c6
Nolen Johnson <johnsonnolen@gmail.com>
2020-12-08 13:06:36 -0500

    [2/2] fixup! sdm660-common: Cleanup dead targets
    Change-Id: I85f084569bf46d80aff625f0db4dd742b72b4a5a

--- sdm660.mk
+++ sdm660.mk
@@ -104,6 +104,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.service \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
