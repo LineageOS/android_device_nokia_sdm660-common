20178e6d1a65813e22d5b3ebd0aeec1cb6a12906
Arne Coucheron <arco68@gmail.com>
2021-01-12 05:45:19 +0100

    sdm660-common: Silence clang warnings during kernel compile
    clang-11: warning: argument unused during compilation:
    '-fuse-ld=lld' [-Wunused-command-line-argument]
    
    Change-Id: I66a629f3573e3fad8aa085ea115475f02d60a7ea

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -94,6 +94,8 @@ BOARD_KERNEL_PAGESIZE := 4096
 TARGET_KERNEL_SOURCE := kernel/nokia/sdm660
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := r383902b
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
