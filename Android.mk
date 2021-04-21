593f93b0301685470f4f8f4e774644b9c3401008
Nolen Johnson <johnsonnolen@gmail.com>
2021-04-21 17:59:32 -0400

    sdm660-common: Add DDV to guards
    Change-Id: I3674ce146e5b34040636a225894acb8e0e6afde1

--- Android.mk
+++ Android.mk
@@ -20,7 +20,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter DRG PL2,$(TARGET_DEVICE)),)
+ifneq ($(filter DDV DRG PL2,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
