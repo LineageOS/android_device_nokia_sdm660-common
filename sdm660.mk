59a810539776bc3afc7c4502f343c67e87b67ca5
Alexander Koskovich <akoskovich@pm.me>
2021-09-10 20:38:25 -0700

    sdm660-common: Remove vendor RenderScript implementation.
     * RenderScript is deprecated on newer platforms and
       is being officially replaced.
    
     * On April 19, 2021, Google announced that RenderScript
       will be deprecated in Android 12, and recommended
       porting existing code to Vulkan.
    
       https://android-developers.googleblog.com/2021/04/android-gpu-compute-going-forward.html
    
    Change-Id: I19460ef266a646b046f1e7d2f0b4eab7c48ae536

--- sdm660.mk
+++ sdm660.mk
@@ -375,10 +375,6 @@ PRODUCT_PACKAGES += \
     init.qti.qseecomd.sh \
     wlan_carrier_bin.sh
 
-# RenderScript
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
