e66388a633e6b351e7aa49c078b92b0f3e8f9ecc
Jeferson Oliveira <jroliveira.oliveira301@gmail.com>
2021-10-04 22:24:13 +0200

    sdm660-common: Squashed removal of TetheringOverlay
    * This was used to disable BPF, which we now support.
    
    This is a squashed commit of:
    
        Revert "sdm660-common: Install TetheringOverlay into vendor"
    
        This reverts commit a16e4d3f0cbd07d33db88ae8b16b3d531132cc10.
    
        Change-Id: Idbfc7b6c3d32ecd9f72a2e184dd661c9c281939b
    
        Revert "sdm660-common: Sign Tether RROs with default cert"
    
        This reverts commit 7d91ea8526cd5c66a455f0da6205b89895c2ebb3.
    
        Change-Id: I5a87d1c678301517f9d04bca7d89812b005fb312
    
        Revert "sdm660-common: TetheringOverlay: Disable BPF"
    
        This reverts commit 8285ea31e4836cd988009907e6b9aec6e3a3674c.
    
        Change-Id: Icdd884e65d3ad9ce2c8e577b4d34a3ae380e48a5
    
    ffk
    
    Change-Id: I067e41645c5c39bd3a60e9462920474d56e2e6f8

--- sdm660.mk
+++ sdm660.mk
@@ -448,7 +448,6 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     libwpa_client \
     hostapd \
-    TetheringConfigOverlay \
     WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
