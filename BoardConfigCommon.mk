d07e5a94a28890eca9d69cfea354260ac1b67a9e
Chenyang Zhong <zhongcy95@gmail.com>
2022-02-16 04:38:45 -0500

    sdm660-common: Specify androidboot.boot_devices
    So that /dev/block/by-name/ symlinks are created.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I270c1899e4487ed5e7657038c4ed07e407476953

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,7 +79,7 @@ BOARD_KERNEL_CMDLINE := \
     console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom \
     msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 \
     service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 \
-    loop.max_part=7
+    loop.max_part=7 androidboot.boot_devices=soc/c0c4000.sdhci
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_KERNEL_SOURCE := kernel/nokia/sdm660
