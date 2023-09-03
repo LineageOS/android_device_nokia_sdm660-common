022d537e573504da842fd04072044cd43abe3b5c
GlowingScrewdriver <vedanthpadmaraman0566@gmail.com>
2023-09-03 06:58:56 +0000

    sdm660-common: Implement FastCharge HAL
    This change implements LineageOS's FastCharge HAL for Nokia SDM660
    devices.
    Tested on PL2 (Nokia 6.1), but it should work for the other Nokia SDM660
    as well since they share the same kernel.
    
    FastCharge HAL Interface definition:
    https://github.com/LineageOS/android_hardware_lineage_interfaces/tree/lineage-20.0/fastcharge/1.0
    
    Change-Id: I78ce842ef6263c19088f29a40abfa1ef6534cc99

--- sdm660.mk
+++ sdm660.mk
@@ -200,6 +200,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
+# Fast Charge
+PRODUCT_PACKAGES += \
+   vendor.lineage.fastcharge@1.0-service
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
