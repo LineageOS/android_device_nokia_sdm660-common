9351f9695e5eee85e0c903b883f1f33a69fb9001
LuK1337 <priv.luk@gmail.com>
2023-10-31 19:06:44 +0100

    sdm660-common: Get rid of AVB 1.0
    It's dead in U.
    
    Change-Id: Id948fe50fd358837cafa3b1e18cf2adba32b927b

--- sdm660.mk
+++ sdm660.mk
@@ -430,11 +430,6 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
-# Verity
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/system
-PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/vendor
-$(call inherit-product, build/target/product/verity.mk)
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
