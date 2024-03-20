232911f650621371fd5c8f58a246b9b7e33992b0
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    sdm660-common: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- common.mk
+++ common.mk
@@ -16,9 +16,6 @@
 
 $(call inherit-product, vendor/nokia/sdm660-common/sdm660-common-vendor.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
