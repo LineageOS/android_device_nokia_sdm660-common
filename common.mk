e700857d8a0147276ecb4fcd4b6e3a238313f93b
Michael Bestas <mikeioannina@gmail.com>
2024-06-14 00:26:49 +0300

    sdm660-common: Build missing libraries for 14 QPR3
    Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2

--- common.mk
+++ common.mk
@@ -153,7 +153,8 @@ PRODUCT_PACKAGES += \
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0.vendor
+    android.hardware.gatekeeper@1.0.vendor \
+    libion.vendor
 
 # GMS
 ifeq ($(WITH_GMS),true)
@@ -263,7 +264,8 @@ PRODUCT_COPY_FILES += \
 # netmgrd
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
-    android.system.net.netd@1.1.vendor
+    android.system.net.netd@1.1.vendor \
+    libnetutils.vendor
 
 # Network
 PRODUCT_PACKAGES += \
@@ -365,6 +367,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     librmnetctl \
+    libsqlite.vendor \
+    libsysutils.vendor \
     libxml2
 
 # Seccomp
