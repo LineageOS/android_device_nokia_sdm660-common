66c613534d7a7326aac29b6c4a231e013086d095
Georg Veichtlbauer <georg@vware.at>
2024-06-25 23:02:34 +0200

    sdm660-common: Build vendor variant of libpower
    Change-Id: Id7fe8a2598ffb93be4635424c86100a88aea8780

--- common.mk
+++ common.mk
@@ -376,7 +376,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    libpower.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf \
