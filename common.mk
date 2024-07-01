c2adace2465869a62677a8f4218f59fbaeec9198
Nolen Johnson <johnsonnolen@gmail.com>
2024-07-01 13:01:50 -0400

    Revert "sdm660-common: Switch to QTI vibrator HAL"
    This reverts commit 849c307ce7c7cce2382a5adb559e0fb2a68b3fcd.
    
    Change-Id: Ief23de2c75c2025b12a2cb8d0da626bec1267050

--- common.mk
+++ common.mk
@@ -416,10 +416,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # Weaver
 PRODUCT_PACKAGES += \
