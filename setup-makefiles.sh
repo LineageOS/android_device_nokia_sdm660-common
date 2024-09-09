c674280a276756a8dd766344a82684755066ea0b
Michael Bestas <mkbestas@lineageos.org>
2024-09-10 02:07:58 +0300

    sdm660-common: extract-files improvements
    Change-Id: I517698ca3b5a93e10a5b167417631c7f8d0ab9e2

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -28,7 +28,7 @@ setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 write_headers "DDV DRG PL2"
 
 # The standard common blobs
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
+write_makefiles "${MY_DIR}/proprietary-files.txt"
 
 # Finish
 write_footers
@@ -41,7 +41,7 @@ if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     write_headers
 
     # The standard device blobs
-    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
+    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt"
 
     if [ -f "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-firmware.txt" ]; then
         append_firmware_calls_to_makefiles "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-firmware.txt"
