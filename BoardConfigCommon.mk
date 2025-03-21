17e7f5da3df33181d35646292f0a990267c3b303
Ratoriku <a1063021545@gmail.com>
2025-03-21 11:24:51 +0700

    sdm660-common: Remove sched_enable_hmp flag
    This flag was already abandoned by CAF in commit 475820b "sched: Remove sched_enable_hmp flag".
    
    Change-Id: I42a683e09885a4389d73e7955177932c18b9667d
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,9 +67,9 @@ TARGET_FS_CONFIG_GEN := $(PLATFORM_PATH)/config.fs
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 androidboot.configfs=true
-BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 sched_enable_power_aware=1
-BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a800000.dwc3
-BOARD_KERNEL_CMDLINE += loop.max_part=7 androidboot.boot_devices=soc/c0c4000.sdhci
+BOARD_KERNEL_CMDLINE += sched_enable_power_aware=1 androidboot.usbcontroller=a800000.dwc3
+BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=1 loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/c0c4000.sdhci
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
