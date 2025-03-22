0ead436cf6c6dca1023429acfed787fd9753e792
Tuan Anh <tuan73176@gmail.com>
2025-03-23 00:04:35 +0700

    sdm660-common: Drop prebuilt audio binaries
    While at it, decommonize ADSP stack.
    
    Change-Id: I44235c21913c1ebc480bcf3d08eb3400660d68a2

--- common.mk
+++ common.mk
@@ -40,6 +40,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.1-impl \
+    audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
