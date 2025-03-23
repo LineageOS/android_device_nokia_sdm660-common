697e9507d00204b7a32faecae9d24a501ac2fa4a
Tuan Anh <tuan73176@gmail.com>
2025-03-23 13:06:00 +0700

    Revert "sdm660-common: Implement FastCharge HAL"
    Iafa7ce24b9f82f44f03dd3ff04bef72dda59cdb6 brings back prebuilt Health HAL,
    so this is not needed anymore. Also the fastcharge implementation is kinda
    janky on this platform, so this will be dropped until we come up with something
    better. A toggle is no longer needed just for fast charging anymore, as this will
    now be handled by the Health HAL.
    
    This reverts commit 1d06c0590b35ed2ac8be5248193a48de0ac54b42.
    
    Change-Id: Ifae7aa9e4b3e815abad90ffb034bf1e114605aa7

--- common.mk
+++ common.mk
@@ -134,10 +134,6 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service \
     android.hardware.biometrics.fingerprint@2.1.vendor
 
-# Fast Charge HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.0-service.qcom
-
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
