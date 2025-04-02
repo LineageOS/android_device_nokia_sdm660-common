7a2efb32926d9f2db2f2d720738de54af3a6bdda
Tuan Anh <tuan73176@gmail.com>
2025-04-02 19:52:25 +0700

    sdm660-common: Drop sensor_def_qcomdev.conf
    Should be device specific, especially for FIH devices where they
    just make everything very confusing.
    
    Change-Id: I17cfbff1f3d3a3b1aabff7a6646d7f50ee4d9b43

--- common.mk
+++ common.mk
@@ -319,7 +319,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Soong
