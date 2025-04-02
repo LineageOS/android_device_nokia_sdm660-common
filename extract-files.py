8dfaf6430b69a0d69336d74273e2d0524dd31b6f
Tuan Anh <tuan73176@gmail.com>
2025-04-02 08:30:13 +0700

    sdm660-common: Decommonize libadsp_hvx* blobs
    To get rid of DISABLE_CHECKELF.
    
    Change-Id: Ie268fcfc6c0039aad42d4f17388ae068e40861bf

--- extract-files.py
+++ extract-files.py
@@ -39,9 +39,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.hardware.fm@1.0',
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
-    (
-	'libmmcamera_dbg',
-    ): lib_fixup_remove,
 }
 
 blob_fixups: blob_fixups_user_type = {
