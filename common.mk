e315907c3d85b18b61657443491ee1e2d3284794
Luofan Chen <clfbbn@gmail.com>
2025-04-02 11:08:08 +0700

    sdm660-common: Move to common Lineage fingerprint AIDL
    Change-Id: I8bb34a7856846a5e34466b838ab24b6bd53ca623

--- common.mk
+++ common.mk
@@ -115,8 +115,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service \
-    android.hardware.biometrics.fingerprint@2.1.vendor
+    android.hardware.biometrics.fingerprint-service.lineage
 
 # FM
 PRODUCT_PACKAGES += \
