c608fc0f4191ea7e084a1b708324c7d2f6811ee0
BladeRunner-A2C <creator4983@aospa.co>
2025-05-14 22:35:42 +0700

    sdm660-common: Remove more unused libraries
    Change-Id: I4e844a68e94f96e0067e9f05445ced6ecdaa8ede

--- extract-files.py
+++ extract-files.py
@@ -46,7 +46,7 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     ('vendor/lib64/mediadrm/libwvdrmengine.so','vendor/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
-    ('vendor/lib/hw/android.hardware.health@2.0-impl-2.1.so', 'vendor/lib64/hw/android.hardware.health@2.0-impl-2.1.so'): blob_fixup()
+    'vendor/lib64/hw/android.hardware.health@2.0-impl-2.1.so': blob_fixup()
         .add_needed('libbase_shim.so'),
     'vendor/etc/izat.conf': blob_fixup()
         .patch_file('gps/0001-gps-izat-Disable-slim_daemon.patch'),
