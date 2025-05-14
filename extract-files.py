73a430ab03c3cccbc5e273a12de0914873054cf1
BladeRunner-A2C <creator4983@aospa.co>
2025-05-14 20:36:35 +0200

    sdm660-common: Use regex to disable slim daemon process stat
    Change-Id: I61220a9bbbd7a095e14f1854b6d6eb2b070440c4

--- extract-files.py
+++ extract-files.py
@@ -49,7 +49,7 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/hw/android.hardware.health@2.0-impl-2.1.so': blob_fixup()
         .add_needed('libbase_shim.so'),
     'vendor/etc/izat.conf': blob_fixup()
-        .patch_file('gps/0001-gps-izat-Disable-slim_daemon.patch'),
+        .regex_replace('PROCESS_STATE=ENABLED', 'PROCESS_STATE=DISABLED'),
     'system_ext/lib64/lib-imscamera.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
