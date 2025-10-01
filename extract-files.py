d3590bf41108d97d5b9403708d3a325b9408fdf9
Tuan Anh <tuan73176@gmail.com>
2025-10-01 14:02:48 +0000

    sdm660-common: Fix indentation in extract-files script
    Change-Id: Ib3dce5ef7ffb0205c541e17f8c74e4493e396f08

--- extract-files.py
+++ extract-files.py
@@ -50,8 +50,8 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/lib-imscamera.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
-	.add_needed('libgui_shim.so')
-	.replace_needed('libqdMetaData.so', 'libqdMetaData.system.so'),
+        .add_needed('libgui_shim.so')
+        .replace_needed('libqdMetaData.so', 'libqdMetaData.system.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
