9d4e630aa3bbf60e4840e32888eaeca89414b2ac
Tuan Anh <tuan73176@gmail.com>
2026-02-21 14:59:46 +0000

    sdm660-common: Copy WLAN config to the correct path
    This will avoid duplicate rules error during build time because of
    the config is copied to the symlinked directory instead of the
    directory it should be copied to (see Android.bp).
    
    Change-Id: I2088560fac36a303cc078af8a515c22fa7a989ff

--- common.mk
+++ common.mk
@@ -327,7 +327,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
