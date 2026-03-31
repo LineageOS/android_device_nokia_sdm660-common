1cb73fcb48bac4c52778154ccc65b5d7469ff56a
Tuan Anh <tuan73176@gmail.com>
2026-03-31 07:09:02 +0000

    sdm660-common: Remove redundant Megvii libs
    Now working without any Megvii deps ^^
    
    Change-Id: Iebf93d1124eaaf306816411977ed1227a5b7e4f4
    (cherry picked from commit 134f79f28d16a4d20bc5892c2f20227e2eba4569)
    (cherry picked from commit a6525462ed579909663b03d84ad7e40cf45d1e66)
    (cherry picked from commit d9a97ada55675702f73100acfa07a3fc7f9104d3)
    (cherry picked from commit 64ec5994c5b796c8510ce58414d871f5f6373ba3)
    (cherry picked from commit 1cbfdcd53e393b7247b178ffb91778703a92c1a1)
    (cherry picked from commit 76502db2a07d8f792ff5971737a330162f6b64d5)
    (cherry picked from commit 90e4c3612bd599a02067b8021a48b089ee7d3f99)

--- extract-files.sh
+++ extract-files.sh
@@ -61,13 +61,13 @@ function blob_fixup() {
     case "${1}" in
     vendor/lib64/hw/camera.sdm660.so)
         patchelf --remove-needed "libMegviiFacepp.so" "${2}"
+        patchelf --remove-needed "libMGBeauty.so" "${2}"
         patchelf --remove-needed "libmegface-new.so" "${2}"
         patchelf --add-needed "libshim_megvii.so" "${2}"
         ;;
     # Fix xml version
     product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml|product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
         sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
-        ;;
     esac
 }
 
